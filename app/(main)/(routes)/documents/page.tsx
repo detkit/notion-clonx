8000a0f410c7211884472d891b3b310d4cb5a532
Dang Quang <quangthat97@gmail.com>
2023-10-25 01:41:52 +0700

    create model schema database

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,13 +1,27 @@
 'use client';
 
 import { useUser } from '@clerk/clerk-react';
+import { useMutation } from 'convex/react';
 import { PlusCircle } from 'lucide-react';
 import Image from 'next/image';
+import { toast } from 'sonner';
 
 import { Button } from '@/components/ui/button';
+import { api } from '@/convex/_generated/api';
 
 const DocumentsPage = () => {
 	const { user } = useUser();
+	const create = useMutation(api.documents.create);
+
+	const onCreate = () => {
+		const promise = create({ title: 'Untitled' });
+
+		toast.promise(promise, {
+			loading: 'Creating a new note...',
+			success: 'New note a created',
+			error: 'Failed to create new note',
+		});
+	};
 
 	return (
 		<div className='flex flex-col items-center justify-center h-full space-y-4'>
@@ -29,7 +43,7 @@ const DocumentsPage = () => {
 			<h2 className='text-lg font-medium'>
 				Welcome to {user?.firstName}&apos;s Jotino
 			</h2>
-			<Button>
+			<Button onClick={onCreate}>
 				<PlusCircle className='w-4 h-4 mr-2' />
 				Create a note
 			</Button>
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,16 +1,19 @@
 'use client';
 
+import { useQuery } from 'convex/react';
 import { ChevronsLeft, MenuIcon } from 'lucide-react';
 import { usePathname } from 'next/navigation';
 import { ElementRef, useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
 
+import { api } from '@/convex/_generated/api';
 import { cn } from '@/lib/utils';
 import UserItem from './user-item';
 
 export default function Navigation() {
 	const pathName = usePathname();
 	const isMobile = useMediaQuery('(max-width: 768px)');
+	const documents = useQuery(api.documents.get);
 	const isResizingRef = useRef(false);
 	const sidebarRef = useRef<ElementRef<'aside'>>(null);
 	const navbarRef = useRef<ElementRef<'div'>>(null);
@@ -119,7 +122,9 @@ export default function Navigation() {
 					<UserItem />
 				</div>
 				<div className='mt-4'>
-					<p>Documents</p>
+					{documents?.map((document) => (
+						<p>{document.title}</p>
+					))}
 				</div>
 
 				<div
