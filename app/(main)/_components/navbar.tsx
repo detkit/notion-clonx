ad526f2bb8c785d36074639e08756c69a96bf8d6
Dang Quang <quangthat97@gmail.com>
2023-10-26 05:01:30 +0700

    menu banner

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -8,6 +8,7 @@ import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel';
 
 import Banner from './banner';
+import Menu from './menu';
 import Title from './title';
 
 interface NavbarProps {
@@ -23,8 +24,11 @@ export default function Navbar({ isCollapsed, onResetSide }: NavbarProps) {
 
 	if (document === undefined) {
 		return (
-			<nav className='bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center'>
+			<nav className='bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center justify-between'>
 				<Title.Skeleton />
+				<div className='flex items-center gap-x-2'>
+					<Menu.Skeleton />
+				</div>
 			</nav>
 		);
 	}
@@ -45,6 +49,9 @@ export default function Navbar({ isCollapsed, onResetSide }: NavbarProps) {
 				)}
 				<div className='flex items-center justify-between w-full'>
 					<Title initialData={document} />
+					<div className='flex items-center gap-x-2'>
+						<Menu documentId={document._id} />
+					</div>
 				</div>
 			</nav>
 
