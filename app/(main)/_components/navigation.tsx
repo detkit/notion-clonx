23213ff04690f5d8e0810766d1dc76be9ef0bbe5
Dang Quang <quangthat97@gmail.com>
2023-10-25 14:59:23 +0700

    create add item and  trash box item

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,29 +1,37 @@
 'use client';
 
-import { useMutation, useQuery } from 'convex/react';
+import { useMutation } from 'convex/react';
 import {
 	ChevronsLeft,
 	MenuIcon,
+	Plus,
 	PlusCircle,
 	Search,
 	Settings,
+	Trash,
 } from 'lucide-react';
 import { usePathname, useRouter } from 'next/navigation';
 import { ElementRef, useEffect, useRef, useState } from 'react';
 import { toast } from 'sonner';
 import { useMediaQuery } from 'usehooks-ts';
 
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from '@/components/ui/popover';
 import { api } from '@/convex/_generated/api';
 import { cn } from '@/lib/utils';
+
 import { DocumentList } from './document-list';
 import { Item } from './item';
+import TrashBox from './trash-box';
 import UserItem from './user-item';
 
 export default function Navigation() {
 	const pathName = usePathname();
 	const router = useRouter();
 	const isMobile = useMediaQuery('(max-width: 768px)');
-	const documents = useQuery(api.documents.get);
 	const create = useMutation(api.documents.create);
 
 	const isResizingRef = useRef(false);
@@ -159,6 +167,22 @@ export default function Navigation() {
 				</div>
 				<div className='mt-4'>
 					<DocumentList />
+					<Item
+						onClick={handleCreate}
+						icon={Plus}
+						label='Add a page'
+					/>
+					<Popover>
+						<PopoverTrigger className='w-full mt-4'>
+							<Item label='Trash' icon={Trash} />
+						</PopoverTrigger>
+						<PopoverContent
+							className='p-0 w-72'
+							side={isMobile ? 'bottom' : 'right'}
+						>
+							<TrashBox />
+						</PopoverContent>
+					</Popover>
 				</div>
 
 				<div
---
+++ app/(main)/_components/trash-box.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function TrashBox() {
+	return <div>Trash Box</div>;
+}
