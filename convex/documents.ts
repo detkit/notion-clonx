068d87bbb15fbf3e95ca1696cfe1ab3c985bb670
Dang Quang <quangthat97@gmail.com>
2023-10-25 14:08:57 +0700

    complete archive functionally

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,5 +1,6 @@
 import { v } from 'convex/values';
 
+import { Id } from './_generated/dataModel';
 import { mutation, query } from './_generated/server';
 
 export const getSidebar = query({
@@ -25,6 +26,54 @@ export const getSidebar = query({
 			.collect();
 
 		return documents;
+	},
+});
+
+export const archive = mutation({
+	args: { id: v.id('document') },
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
+
+		if (!identity) {
+			throw new Error('Not authenticated');
+		}
+
+		const userId = identity.subject;
+
+		const existingDocument = await ctx.db.get(args.id);
+
+		if (!existingDocument) {
+			throw new Error('Not Found');
+		}
+
+		if (existingDocument.userId !== userId) {
+			throw new Error('Unauthorized');
+		}
+
+		const recusiveArchive = async (documentId: Id<'document'>) => {
+			const children = await ctx.db
+				.query('document')
+				.withIndex('by_user_parent', (q) =>
+					q.eq('userId', userId).eq('parentDocument', documentId)
+				)
+				.collect();
+
+			for (const child of children) {
+				await ctx.db.patch(child._id, {
+					isArchived: true,
+				});
+
+				await recusiveArchive(child._id);
+			}
+		};
+
+		const document = await ctx.db.patch(args.id, {
+			isArchived: true,
+		});
+
+		recusiveArchive(args.id);
+
+		return document;
 	},
 });
 
