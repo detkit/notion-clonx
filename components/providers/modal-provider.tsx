61c068cacd3be2850d27abb108bf9366ad7d3ef0
Dang Quang <quangthat97@gmail.com>
2023-10-26 12:47:52 +0700

    create on publish

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 
 import { SettingsMd } from '@/components/modals/SettingsMd';
+import { CoverImageModal } from '@/components/modals/cover-image-modal';
 
 export const ModalProvider = () => {
 	const [isMounted, setIsMounted] = useState(false);
@@ -18,6 +19,7 @@ export const ModalProvider = () => {
 	return (
 		<>
 			<SettingsMd />
+			<CoverImageModal />
 		</>
 	);
 };
