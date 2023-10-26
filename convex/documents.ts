950c690e7c090e38b268d3b831a0f24bc878c48b
Dang Quang <quangthat97@gmail.com>
2023-10-26 09:58:44 +0700

    first

--- convex/documents.ts
+++ convex/documents.ts
@@ -249,14 +249,10 @@ export const getById = query({
 	handler: async (ctx, args) => {
 		const identity = await ctx.auth.getUserIdentity();
 
-		if (!identity) {
-			throw new Error('Not authenticated');
-		}
-
 		const document = await ctx.db.get(args.documentId);
 
 		if (!document) {
-			throw new Error('Not Found');
+			throw new Error('Not found');
 		}
 
 		if (document.isPublished && !document.isArchived) {
@@ -309,6 +305,64 @@ export const update = mutation({
 
 		const document = await ctx.db.patch(args.id, {
 			...rest,
+		});
+
+		return document;
+	},
+});
+
+export const removeIcon = mutation({
+	args: { id: v.id('document') },
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
+
+		if (!identity) {
+			throw new Error('Unauthenticated');
+		}
+
+		const userId = identity.subject;
+
+		const existingDocument = await ctx.db.get(args.id);
+
+		if (!existingDocument) {
+			throw new Error('Not found');
+		}
+
+		if (existingDocument.userId !== userId) {
+			throw new Error('Unauthorized');
+		}
+
+		const document = await ctx.db.patch(args.id, {
+			icon: undefined,
+		});
+
+		return document;
+	},
+});
+
+export const removeCoverImage = mutation({
+	args: { id: v.id('document') },
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
+
+		if (!identity) {
+			throw new Error('Unauthenticated');
+		}
+
+		const userId = identity.subject;
+
+		const existingDocument = await ctx.db.get(args.id);
+
+		if (!existingDocument) {
+			throw new Error('Not found');
+		}
+
+		if (existingDocument.userId !== userId) {
+			throw new Error('Unauthorized');
+		}
+
+		const document = await ctx.db.patch(args.id, {
+			coverImage: undefined,
 		});
 
 		return document;
